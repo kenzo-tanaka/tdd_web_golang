c29332b25ea538cbed8c6cd664e827ebd9acb054
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-02-05 17:32:18 +0900

    Add Hello, World pattern

--- hello.go
+++ hello.go
@@ -3,5 +3,8 @@ package main
 const englishHelloPrefix = "Hello, "
 
 func Hello(name string) string {
+	if name == "" {
+		return englishHelloPrefix + "World"
+	}
 	return englishHelloPrefix + name
 }
