d925927020f3d89b824e391b8276f925d4aded35
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-02-05 17:29:48 +0900

    Add t.Run statement

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,11 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+	t.Run("saying hello to peaple", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
